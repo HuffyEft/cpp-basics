e8ecb5bbdcfb8e9dcb91834e7f2b9b3c8ffa8b47
huffyeft <huffyeft@greenmail.net>
2018-10-26 14:19:39 +0300

    -_-

--- 02-conditionals-loops-1/main.cpp
+++ 02-conditionals-loops-1/main.cpp
@@ -4,9 +4,6 @@
 
 using namespace std;
 
-void print(double, double);
-bool isReal;
-
 int main() {
 	double a, b, c, xS, xF, dx, F;
 	cout << "Enter a, b, c, xS, xF, dx" << endl;
@@ -21,31 +18,27 @@ int main() {
 		cout << "Invalid xf";
 	}
 	else {
-		if ((static_cast<int>(a) & (static_cast<int>(b) | static_cast<int>(c))) != 0) {
-			isReal = true;
-		}
-		int i = xS;
-		for (i; i <= xF; i = i + dx) {
+		for (int i = xS; i <= xF; i = i + dx) {
 			if (a < 0 && (c != 0)) {
 				F = a * pow(i, 2) + (b * i) + c;
 			}
 			else if (a > 0 && c == 0) {
+				if ((i - c) == 0) {
+					cout << "division by 0\n";
+					continue;
+				}
 				F = (-a / (i - c));
 			}
 			else {
 				F = a * (i + c);
 			}
-			print(xS, F);
+			if ((static_cast<int>(a) & (static_cast<int>(b) | static_cast<int>(c))) != 0) {
+				F = static_cast<int>(F);
+			}
+			cout << string(33, '_') << endl
+				<< "|" << setw(8) << xS << setw(8) << "|"
+				<< setw(8) << F << setw(8) << "|" << endl;
 		}
 		cout << string(33, '-');
 	}
-}
-
-void print(double x, double F) {
-	if (isReal) {
-		F = static_cast<int>(F);
-	}
-	cout << string(33, '_') << endl
-		<< "|" << setw(8) << x << setw(8) << "|"
-		<< setw(8) << F << setw(8) << "|" << endl;
 }
