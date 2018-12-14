d2fc0c8f9bc776ae73afcb9842a3abfeecc66c9c
huffyeft <m0d.0w@mail.ru>
2018-12-14 17:46:51 +0300

    major updates

--- 01-linear-program/main.cpp
+++ 01-linear-program/main.cpp
@@ -1,19 +1,17 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-void main() {
-
-	double a, z1, z2;
-
-	cout << "Enter a ";
-
+int main() {
+	double a;
+	cout << "Enter alpha -> ";
 	cin >> a;
 
-	z1 = (sin(2 * a) + sin(5 * a) - sin(3 * a)) / (cos(a) + 1 - 2 * pow(sin(2 * a), 2));
+	double z1 = (sin(2 * a) + sin(5 * a) - sin(3 * a)) /
+		(cos(a) + 1 - 2 * pow(sin(2 * a), 2));
+	double z2 = 2 * sin(a);
+	cout << "\nz1= " << z1 << "\nz2= " << z2 << endl;
 
-	z2 = 2 * sin(a);
-
-	cout << "z1 = " << z1 << endl << "z2 = " << z2 << endl;
-
+	return 0;
 }
--- 02-conditionals-loops-1/main.cpp
+++ 02-conditionals-loops-1/main.cpp
@@ -1,44 +1,75 @@
 #include <iostream>
-#include <string>
 #include <iomanip>
+#include <string>
 
 using namespace std;
 
 int main() {
-	double a, b, c, xS, xF, dx, F;
-	cout << "Enter a, b, c, xS, xF, dx" << endl;
-	cin >> a >> b >> c >> xS >> xF >> dx;
-	cout << string(33, '-') << endl << "|" << setw(8) << "X" << setw(8) << "|"
-		<< setw(8) << "F" << setw(8) << "|" << endl;
+	const double kEps = 1e-15;
+
+	double a, b, c, xn, xk, dx;
+	cout << "Enter a -> ";
+	cin >> a;
+	cout << "Enter b -> ";
+	cin >> b;
+	cout << "Enter c -> ";
+	cin >> c;
+	cout << "Enter xn -> ";
+	cin >> xn;
+	cout << "Enter xk (xk >= xn) -> ";
+	cin >> xk;
+	cout << "Enter dx (dx > 0) -> ";
+	cin >> dx;
 
 	if (dx <= 0) {
-		cout << "Invalid dx";
+		cout << "\nInvalid dx. Must be: dx > 0.\n";
 	}
-	else if (xS > xF) {
-		cout << "Invalid xf";
+	else if (xn > xk) {
+		cout << "\nInvalid xk. Must be: xk >= xn.\n";
 	}
 	else {
-		for (int i = xS; i <= xF; i = i + dx) {
-			if (a < 0 && (c != 0)) {
-				F = a * pow(i, 2) + (b * i) + c;
+		cout << string(37, '-') << endl;
+		cout << "|        X        |        F        |\n";
+		cout << string(37, '-') << endl;
+
+		cout << fixed;
+		cout.precision(3);
+
+		while (xn <= xk) {
+			cout << "|" << setw(11) << xn << setw(7) << "|";
+
+			double f;
+			if ((a < 0) && !(abs(c) < kEps)) {
+				f = a * xn * xn + b * xn + c;
 			}
-			else if (a > 0 && c == 0) {
-				if ((i - c) == 0) {
-					cout << "division by 0\n";
+			else if ((a > 0) && (abs(c) < kEps)) {
+				if (abs(xn - c) < kEps) {
+					cout << "  division by 0  |\n";
+					xn += dx;
 					continue;
 				}
-				F = (-a / (i - c));
+				else {
+					f = -a / (xn - c);
+				}
 			}
 			else {
-				F = a * (i + c);
+				f = a * (xn + c);
 			}
-			if ((static_cast<int>(a) & (static_cast<int>(b) | static_cast<int>(c))) != 0) {
-				F = static_cast<int>(F);
-			}
-			cout << string(33, '_') << endl
-				<< "|" << setw(8) << xS << setw(8) << "|"
-				<< setw(8) << F << setw(8) << "|" << endl;
+
+			int ac = static_cast<int>(a);
+			int bc = static_cast<int>(b);
+			int cc = static_cast<int>(c);
+
+			cout << setw(11);
+			if ((ac & (bc | cc)) != 0)
+				cout << f;
+			else
+				cout << static_cast<int>(f);
+			cout << setw(8) << "|\n";
+
+			xn += dx;
 		}
-		cout << string(33, '-');
+		cout << string(37, '-');
 	}
+	return 0;
 }
